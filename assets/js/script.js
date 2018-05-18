6401112d7a6c2c601b18d2473af5af3c627e8a47
JR Sockwell <jr@jrsockwell.com>
2018-05-18 15:05:39 -0700

    Added Lots of JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,47 @@
 var request = new XMLHttpRequest();
+var symbol = 'BTC';
+var currency = 'USD';
+getPrice();
 
-request.open('GET', 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP&tsyms=USD', true);
-request.onload = function () {
 
-  var data = JSON.parse(this.response);
-  var symbol = 'ETH'
-  document.getElementById("price").innerHTML = data[symbol]["USD"];
-  document.getElementById("name").innerHTML = data.BTC.USD;
+function getPrice(){
+  request.open('GET', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,XRP&tsyms=USD', true);
+  request.onload = function () {
+
+    var data = JSON.parse(this.response);
+    var price = data[symbol][currency][PRICE];
+    var volume = data[symbol][currency][VOLUME24H];
+    var highDay = data[symbol][currency][HIGHDAY];
+    var lowDay = data[symbol][currency][LOWDAY];
+    var change24H = data[symbol][currency][CHANGE24HOUR];
+    var changePCT24H = data[symbol][currency][CHANGEPTC24HOUR];
+    var MKTCap = data[symbol][currency][MKTCAP];
+    document.getElementById("price").innerHTML = price;
+
+  };
+
+  request.send();
+
 }
 
-request.send();
+
+
+function changeBTC(){
+  symbol = 'BTC';
+  getPrice();
+}
+
+function changeETH(){
+  symbol = 'ETH';
+  getPrice();
+}
+
+function changeLTC(){
+  symbol = 'LTC';
+  getPrice();
+}
+
+function changeXRP(){
+  symbol = 'XRP';
+  getPrice();
+}
