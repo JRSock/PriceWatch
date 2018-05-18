fc4d8ad3d6c8c573f1db2fec4e6e02dd245c32dd
JR Sockwell <jr@jrsockwell.com>
2018-05-18 16:10:24 -0700

    Fixed JS again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,28 @@
-var request = new XMLHttpRequest();
-var symbol = 'BTC';
-var currency = 'USD';
-getPrice();
-
-
-function getPrice(){
-  request.open('GET', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,XRP&tsyms=USD', true);
-  request.onload = function () {
-
-    var data = JSON.parse(this.response);
-    var price = data["DISPLAY"][symbol][currency]["PRICE"];
-    var volume = data["DISPLAY"][symbol][currency]["VOLUME24HOURTO"];
-    var highDay = data["DISPLAY"][symbol][currency]["HIGHDAY"];
-    var lowDay = data["DISPLAY"][symbol][currency]["LOWDAY"];
-    var change24H = data["DISPLAY"][symbol][currency]["CHANGE24HOUR"];
-    var changePCT24H = data["DISPLAY"][symbol][currency]["CHANGEPCT24HOUR"];
-    var MKTCap = data["DISPLAY"][symbol][currency]["MKTCAP"];
-    document.getElementById("price").innerHTML = price;
-    document.getElementById("volume").innerHTML = volume;
-    document.getElementById("highday").innerHTML = highDay;
-    document.getElementById("lowday").innerHTML = lowDay;
-    document.getElementById("change24h").innerHTML = change24H;
-    document.getElementById("changepct24h").innerHTML = changePCT24H + ' %';
-    document.getElementById("mktcap").innerHTML = MKTCap;
-
-  };
-
-  request.send();
-
-}
+updatePrice("BTC", "USD")
 
 
 
-function changeBTC(){
-  symbol = 'BTC';
-  getPrice();
-}
-
-function changeETH(){
-  symbol = 'ETH';
-  getPrice();
-}
-
-function changeLTC(){
-  symbol = 'LTC';
-  getPrice();
-}
-
-function changeXRP(){
-  symbol = 'XRP';
-  getPrice();
+function updatePrice(symbol, currency) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            var data = JSON.parse(xmlHttp.responseText);
+            var price = data["DISPLAY"][symbol][currency]["PRICE"];
+            var volume = data["DISPLAY"][symbol][currency]["VOLUME24HOURTO"];
+            var highDay = data["DISPLAY"][symbol][currency]["HIGHDAY"];
+            var lowDay = data["DISPLAY"][symbol][currency]["LOWDAY"];
+            var change24H = data["DISPLAY"][symbol][currency]["CHANGE24HOUR"];
+            var changePCT24H = data["DISPLAY"][symbol][currency]["CHANGEPCT24HOUR"];
+            var MKTCap = data["DISPLAY"][symbol][currency]["MKTCAP"];
+            document.getElementById("price").innerHTML = price;
+            document.getElementById("volume").innerHTML = volume;
+            document.getElementById("highday").innerHTML = highDay;
+            document.getElementById("lowday").innerHTML = lowDay;
+            document.getElementById("change24h").innerHTML = change24H;
+            document.getElementById("changepct24h").innerHTML = changePCT24H + ' %';
+            document.getElementById("mktcap").innerHTML = MKTCap;
+        }
+    }
+    xmlHttp.open("GET", 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,XRP&tsyms=USD', true); // true for asynchronous
+    xmlHttp.send(null);
 }
