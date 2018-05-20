784b1ac93fe952b757ac494289aaef4eb4ab586f
JR Sockwell <jr@jrsockwell.com>
2018-05-19 21:45:45 -0700

    Added comments and loading text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
 updatePrice("BTC", "USD")
 
+//Set VARS to the button elements
 var btnbtc = document.getElementById("btnbtc");
 var btneth = document.getElementById("btneth");
 var btnltc = document.getElementById("btnltc");
 var btnxrp = document.getElementById("btnxrp");
 
+//Allow user to click button to change coin
 btnbtc.addEventListener("click", function(){updatePrice("BTC", "USD")});
 btneth.addEventListener("click", function(){updatePrice("ETH", "USD")});
 btnltc.addEventListener("click", function(){updatePrice("LTC", "USD")});
 btnxrp.addEventListener("click", function(){updatePrice("XRP", "USD")});
 
+//Updates price based on coin and users selected currency
 function updatePrice(symbol, currency) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
             var data = JSON.parse(xmlHttp.responseText);
+
+            //Set vars to values taken from API
             var price = data["DISPLAY"][symbol][currency]["PRICE"];
             var volume = data["DISPLAY"][symbol][currency]["VOLUME24HOURTO"];
             var highDay = data["DISPLAY"][symbol][currency]["HIGHDAY"];
@@ -22,6 +27,8 @@ function updatePrice(symbol, currency) {
             var change24H = data["DISPLAY"][symbol][currency]["CHANGE24HOUR"];
             var changePCT24H = data["DISPLAY"][symbol][currency]["CHANGEPCT24HOUR"];
             var MKTCap = data["DISPLAY"][symbol][currency]["MKTCAP"];
+
+            //Change elements to represent values
             document.getElementById("price").innerHTML = price;
             document.getElementById("volume").innerHTML = volume;
             document.getElementById("highday").innerHTML = highDay;
